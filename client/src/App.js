ef33d0c557b1fc63d91db937d504e2e8877e2ce9
Michelle A <michelleasuamah@Michelles-MacBook-Pro.local>
2022-09-08 15:15:45 -0400

    rearranged code/files

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,12 @@
-import { useRef } from "react"
+import VoicePrompt from './components/VoicePrompt';
 import './App.css';
 
 function App() {
 
-  const socket = useRef(null)
-  //const recorder = useRef(null)
-
-  const runApp = async () => {
-
-    const response = await fetch('http://localhost:8000');
-    const data = await response.json();
-
-    if(data.error){
-      alert(data.error)
-    }
-    
-    const { token } = data;
-
-    socket.current = await new WebSocket(`wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&token=${token}`);
-
-    console.log(socket)
-
-  };
 
   return (
     <div className="App">
-        <button onClick={runApp}>hey</button>
+        <VoicePrompt />
     </div>
   );
 }
